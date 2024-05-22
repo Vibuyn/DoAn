146772f8a23821c6d3c27ba079c56d93ca058e7d
namnx <nxnam1102@gmail.com>
2024-05-22 18:01:17 +0700

    Add dashboard with data

---
+++ CoffeeShopMangement/ModelViews/Dashboard.cs
@@ -0,0 +1,35 @@
+﻿using CoffeeShopMangement.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CoffeeShopMangement.ModelViews
+{
+    public class DashboardBestSeller
+    {
+        public int Count { get; set; }
+        public Product Product { get; set; }
+    }
+    public class DashboardRevenueDayMonthYear
+    {
+        public int Month { get; set; }
+        public int PreviousMonth { get; set; }
+        public int Today { get; set; }
+        public int Yesterday { get; set; }
+        public int Year { get; set; }
+        public int PreviousYear { get; set; }
+        public int Total { get; set; }
+        public double PercentToday { get; set; }
+        public double PercentMonth { get; set; }
+        public double PercentYear { get; set; }
+    }
+    public class Dashboard
+    {
+        public List<Order> RecentOrder { get; set; }
+        public List<DashboardBestSeller> BestSeller { get; set; }
+        public DashboardRevenueDayMonthYear DayMonthYear { get; set; }
+        public List<(DateTime,int)> CountOrders { get; set; }
+        public List<(DateTime, int)> SumOrders { get; set; }
+        public List<DashboardBestSeller> ProductsOrderToday { get; set; }
+        public string[] ListColor { get; set; }
+    }
+}
